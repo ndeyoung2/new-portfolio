31c24a85979b77740b5b3416edf27087273f1b33
NatalieRicci <ndeyoung@gmail.com>
2023-03-31 12:47:58 -0700

    more css

--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { motion, useInView, useAnimate } from "framer-motion";
-import { Container, Image } from "react-bootstrap";
+import { Container, Card, Col, Row, Image } from "react-bootstrap";
 
 const Landing = () => {
   const [scope, animate] = useAnimate();
@@ -18,20 +18,30 @@ const Landing = () => {
     <section id="landing">
       <motion.div ref={scope} transition={"spring"} animate={{ scale: 1.2 }}>
         <Container id="landing">
-          <h1 margin={3}>
+          <Col>
+          <h1>
+            <Row>
             Natalie DeYoung Ricci
-            <p></p>
+            </Row>
+            <Row>
             Full Stack Software Engineer
-          </h1>
-          <Image
+            </Row>
+            </h1>
+            </Col>
+            <Col>
+            <Card>
+            <Image
           className="hero"
           src="/assets/hero.png"
-          width={150}
-          height={150}
+          width={200}
+          height={200}
           alt="hero"
           margin="5"
           >
           </Image>
+          </Card>
+</Col>
+
           </Container>
 
       </motion.div>
