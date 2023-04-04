aa01c30b74d073457a518593e25f29f1f79e84c0
NatalieRicci <ndeyoung@gmail.com>
2023-04-04 10:45:49 -0700

    format

--- src/Tech.js
+++ src/Tech.js
@@ -3,7 +3,6 @@ import { Card, Button, Container } from "react-bootstrap";
 import { useAnimate, motion, useInView } from "framer-motion";
 
 const Tech = () => {
-
   const [scope, animate] = useAnimate();
 
   const ref = useRef(null);
@@ -11,10 +10,9 @@ const Tech = () => {
 
   useEffect(() => {
     if (isInView) {
-      animate(scope.current, { scale: 1.1, delay: .5, });
+      animate(scope.current, { scale: 1.1, delay: 0.5 });
     }
   }, [isInView]);
-
 
   const tech = [
     "JavaScript",
@@ -37,26 +35,24 @@ const Tech = () => {
 
   return (
     <>
-      <motion.div ref={scope} transition={'spring'} animate={{scale: 1.1}}>
+      <motion.div ref={scope} transition={"spring"} animate={{ scale: 1.1 }}>
         <Container>
-        <Card  id="techCard">
-          <Card.Title id="tech">
-            Tools and Tech Stack
-          </Card.Title>
-          <Card.Body justify-content="center">
-            Trained in object-oriented programming, I work primarily in
-            JavaScript with the PERN stack, but am always looking to expand my
-            skills. Currently, I am learning Typescript, Next.js, and I have
-            some familiarity with Docker as well.
-            <Card.Text>
-              {tech.map((item) => (
-                <Button className="button" key={item}>
-                  {item}
-                </Button>
-              ))}
-            </Card.Text>
-          </Card.Body>
-        </Card>
+          <Card id="techCard">
+            <Card.Title id="tech">Tools and Tech Stack</Card.Title>
+            <Card.Body justify-content="center">
+              Trained in object-oriented programming, I work primarily in
+              JavaScript with the PERN stack, but am always looking to expand my
+              skills. Currently, I am learning Typescript, Next.js, and I have
+              some familiarity with Docker as well.
+              <Card.Text>
+                {tech.map((item) => (
+                  <Button className="button" key={item}>
+                    {item}
+                  </Button>
+                ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Container>
       </motion.div>
     </>
