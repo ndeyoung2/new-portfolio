46307f7f31dc942d3b77298dac7d384fa31d7664
NatalieRicci <ndeyoung@gmail.com>
2023-05-19 07:40:05 -0700

    format

--- src/TopNav.js
+++ src/TopNav.js
@@ -5,50 +5,50 @@ import { Nav, Navbar, Container, Image } from "react-bootstrap";
 const TopNav = () => {
   return (
     <>
-        <Navbar className="topnav" id="navbar" variant="pills">
-          <Nav as="ul">
-            <Nav.Link
-              as="li"
-              className="left"
-              href="https://github.com/ndeyoung2"
-            >
-              <Image
-                className="icon"
-                src="assets/github_icon.png"
-                width="20"
-              ></Image>
-            </Nav.Link>
-            <Nav.Link
-              as="li"
-              className="left"
-              href="https://www.linkedin.com/in/natalie-deyoung-ricci/"
-            >
-              <Image
-                className="icon"
-                src="assets/linkedin_icon.webp"
-                width="20"
-              ></Image>
-            </Nav.Link>
-            <Nav.Link as="li" className="right" href="/">
-              <Image className="icon" src="assets/home.png" width="20"></Image>
-            </Nav.Link>
-            <Nav.Link as="li" className="right" href="#about">
-              About
-            </Nav.Link>
-            <Nav.Link as="li" className="right" href="#tech">
-              Tech
-            </Nav.Link>
-            <Nav.Link as="li" className="right" href="#projects">
-              Projects
-            </Nav.Link>
-            <Nav.Link as="li" className="right" href="#writing">
-              Writing
-            </Nav.Link>
-            <Nav.Link as="li" className="right" href="#contact">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar>
+      <Navbar className="topnav" id="navbar" variant="pills">
+        <Nav as="ul">
+          <Nav.Link
+            as="li"
+            className="left"
+            href="https://github.com/ndeyoung2"
+          >
+            <Image
+              className="icon"
+              src="assets/github_icon.png"
+              width="20"
+            ></Image>
+          </Nav.Link>
+          <Nav.Link
+            as="li"
+            className="left"
+            href="https://www.linkedin.com/in/natalie-deyoung-ricci/"
+          >
+            <Image
+              className="icon"
+              src="assets/linkedin_icon.webp"
+              width="20"
+            ></Image>
+          </Nav.Link>
+          <Nav.Link as="li" className="right" href="/">
+            <Image className="icon" src="assets/home.png" width="20"></Image>
+          </Nav.Link>
+          <Nav.Link as="li" className="right" href="#about">
+            About
+          </Nav.Link>
+          <Nav.Link as="li" className="right" href="#tech">
+            Tech
+          </Nav.Link>
+          <Nav.Link as="li" className="right" href="#projects">
+            Projects
+          </Nav.Link>
+          <Nav.Link as="li" className="right" href="#writing">
+            Writing
+          </Nav.Link>
+          <Nav.Link as="li" className="right" href="#contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar>
     </>
   );
 };
